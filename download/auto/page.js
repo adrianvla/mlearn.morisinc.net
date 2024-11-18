0fd9d49db70fa7909347b5c7a4a0ea3137f3c308
adrianvla <admin@morisinc.net>
2024-11-18 15:42:19 +0100

    Updated Download URLs

--- download/auto/page.js
+++ download/auto/page.js
@@ -1,9 +1,9 @@
 //get url hash
 const URLS = {
-    "linux":"https://mlearn.morisinc.net/static/linux/mLearn-linux-x64.zip",
-    "windows":"https://mlearn.morisinc.net/static/win32/mLearn-win32-x64.zip",
-    "macos_x86":"https://mlearn.morisinc.net/static/darwin/mLearn_x86.dmg",
-    "macos_arm64":"https://mlearn.morisinc.net/static/darwin/mLearn_arm64.dmg",
+    "linux":"https://github.com/adrianvla/mLearn/releases/download/release/mLearn-linux-x64.zip",
+    "windows":"https://github.com/adrianvla/mLearn/releases/download/release/mLearn-win32-x64.zip",
+    "macos_x86":"https://github.com/adrianvla/mLearn/releases/download/release/mLearn_x86.dmg",
+    "macos_arm64":"https://github.com/adrianvla/mLearn/releases/download/release/mLearn_arm64.dmg",
 };
 
 
