89f8d9cddbd3fe39e534c68e4c3e6c465061438c
adrianvla <admin@morisinc.net>
2024-11-18 15:16:17 +0100

    Added Software

--- download/auto/page.js
+++ download/auto/page.js
@@ -1,9 +1,9 @@
 //get url hash
 const URLS = {
-    "linux":"https://mlearn.morisinc.net/static/linux/mLearn_linux_x86.zip",
-    "windows":"https://mlearn.morisinc.net/static/win32/mLearn_win32_x86.zip",
-    "macos_x86":"https://mlearn.morisinc.net/static/darwin/mLearn_darwin_x86.zip",
-    "macos_arm64":"https://mlearn.morisinc.net/static/darwin/mLearn_darwin_arm64.zip",
+    "linux":"https://mlearn.morisinc.net/static/linux/mLearn-linux-x64.zip",
+    "windows":"https://mlearn.morisinc.net/static/win32/mLearn-win32-x64.zip",
+    "macos_x86":"https://mlearn.morisinc.net/static/darwin/mLearn_x86.dmg",
+    "macos_arm64":"https://mlearn.morisinc.net/static/darwin/mLearn_arm64.dmg",
 };
 
 
