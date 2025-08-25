64b2f19601b50de632884975b4fec50a9474d900
Adrian <86854740+adrianvla@users.noreply.github.com>
2025-08-25 02:47:45 +0200

    Add files via upload

--- download/auto/page.js
+++ download/auto/page.js
@@ -1,9 +1,8 @@
 //get url hash
 const URLS = {
-    "linux":"https://github.com/adrianvla/mLearn/releases/download/v1.3/mLearn-linux-x64.zip",
-    "windows":"https://github.com/adrianvla/mLearn/releases/download/v1.3/mLearn-win32-x64.zip",
-    "macos_x86":"https://github.com/adrianvla/mLearn/releases/download/v1.3/mLearn-x64.dmg",
-    "macos_arm64":"https://github.com/adrianvla/mLearn/releases/download/v1.3/mLearn-arm64.dmg",
+    "linux":"https://github.com/adrianvla/mLearn/releases/download/release/mLearn-linux-x64.zip",
+    "windows":"https://github.com/adrianvla/mLearn/releases/download/release/mLearn-win32-x64.zip",
+    "macos":"https://github.com/adrianvla/mLearn/releases/download/release/mLearn_universal.dmg",
 };
 
 
