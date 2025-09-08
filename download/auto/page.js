7c972b94edae5481e5f7e4a4ea1b0cb65c1ace66
adrianvla <admin@morisinc.net>
2025-09-08 23:33:23 +0200

    updated to mLearn v1.3.4 download links

--- download/auto/page.js
+++ download/auto/page.js
@@ -1,10 +1,10 @@
 //get url hash
 const URLS = {
-    "linux-standalone":"https://github.com/adrianvla/mLearn/releases/download/v1.3.3/mLearn-1.3.3-linux-amd64.zip",
-    "linux-installer":"https://github.com/adrianvla/mLearn/releases/download/v1.3.3/mlearn_1.3.3_amd64.deb",
-    "windows-standalone":"https://github.com/adrianvla/mLearn/releases/download/v1.3.3/mLearn-1.3.3-win.zip",
-    "windows-installer":"https://github.com/adrianvla/mLearn/releases/download/v1.3.3/mLearn.Setup.1.3.3.exe",
-    "macos":"https://github.com/adrianvla/mLearn/releases/download/v1.3.3/mLearn-1.3.3-mac.dmg",
+    "linux-standalone":"https://github.com/adrianvla/mLearn/releases/download/v1.3.4/mLearn-1.3.4-linux-amd64.zip",
+    "linux-installer":"https://github.com/adrianvla/mLearn/releases/download/v1.3.4/mlearn_1.3.4_amd64.deb",
+    "windows-standalone":"https://github.com/adrianvla/mLearn/releases/download/v1.3.4/mLearn-1.3.4-win.zip",
+    "windows-installer":"https://github.com/adrianvla/mLearn/releases/download/v1.3.4/mLearn.Setup.1.3.4.exe",
+    "macos":"https://github.com/adrianvla/mLearn/releases/download/v1.3.4/mLearn-1.3.4-mac.dmg",
 };
 
 
